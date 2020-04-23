5e759c22727e2718227622fed5eb4311249b20ff
mihanizm56 <m727507.56@mail.ru>
2020-04-24 02:43:03 +0300

    build(configure-package): upd

--- config/packager/executor.js
+++ config/packager/executor.js
@@ -1,80 +1,57 @@
 #!/usr/bin/env node
 
-const path = require('path');
-const Copier = require('@mihanizm56/node-file-copier');
-const { exec, writeFile } = require('./utils/fs-promises');
-const { getConsoleArgs } = require('./utils/get-args')
-const commandsFile = require('./commands/commands.json');
+const path = require("path");
+const Copier = require("@mihanizm56/node-file-copier");
+const { exec } = require("./utils/fs-promises");
+const { getConsoleArgs } = require("./utils/get-args");
+const {
+  packageJsonPatchEU,
+  packageJsonPatchRU,
+} = require("./utils/package-json-patch");
 
 const flags = getConsoleArgs(process.argv);
 
-const configPath = flags['--eu-mod'] ? 'eu' : 'ru';
+const configPath = flags.euro ? "eu" : "ru";
+const packageJsonPatch = flags.euro ? packageJsonPatchEU : packageJsonPatchRU;
 
 const fromFolder = path.join(
   process.cwd(),
-  'node_modules',
-  '@wildberries',
-  'boilerplate-config-packager',
-  'lib',
+  "node_modules",
+  "@wildberries",
+  "boilerplate-config-packager",
+  "lib",
   configPath
 );
 
-const toFolder = path.join(process.cwd(), 'config');
+const toFolder = path.join(process.cwd(), "config");
 
 const arrayToCopy = [{ from: fromFolder, to: toFolder }];
 
 const copier = new Copier({ arrayToCopy });
 
-const addPackageJsonCommands = async () => {
-  try {
-    const { scripts, devDependencies, dependencies } = commandsFile;
-
-    const packageJsonProjectFile = require(path.join(process.cwd(),'package.json')); // eslint-disable-line
-
-    const newPackage = {
-      ...packageJsonProjectFile,
-      scripts,
-      dependencies: {
-        ...packageJsonProjectFile.dependencies,
-        ...dependencies,
-      },
-      devDependencies: {
-        ...packageJsonProjectFile.devDependencies,
-        ...devDependencies,
-      },
-    };
-
-    await writeFile(
-      path.join(process.cwd(), 'package.json'),
-      JSON.stringify(newPackage, null, 2),
-      'utf8',
-    );
-  } catch (error) {
-    console.log('get an error when getting package', error);
-  }
-};
-
 const runPackage = async () => {
   try {
-    console.log('(config-packager): start to execute');
+    console.log("(config-packager): start to execute");
 
-    await exec('npm install @wildberries/boilerplate-config-packager');
+    await exec(
+      "npm install @wildberries/boilerplate-config-packager@0.0.2-beta.10"
+    );
 
-    console.log('(config-packager): start to copy');
+    console.log("(config-packager): start to copy");
 
     copier.activate();
 
-    // console.log('(config-packager): start to patch package.json');
+    console.log("(config-packager): start to patch package.json");
 
-    // await addPackageJsonCommands(); todo add
+    await packageJsonPatch();
 
-    // await exec('npm uninstall @wildberries/boilerplate-config-packager');
+    await exec("npm uninstall @wildberries/boilerplate-config-packager");
 
-    // console.log('(config-packager): start install cli packages');
+    console.log("(config-packager): start install cli packages");
 
-    // await exec('npm install');
+    await exec("npm install");
 
-    console.log('(config-packager): installed successfully');
+    console.log("(config-packager): installed successfully");
   } catch (error) {
     console.log("error when executing the package", error); // eslint-disable-line
   }
