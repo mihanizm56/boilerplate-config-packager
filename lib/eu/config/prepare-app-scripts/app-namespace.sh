dfa7e49c501c820ba6018d76f7d4860dc0970155
mihanizm56 <m727507.56@mail.ru>
2020-05-10 04:09:43 +0300

    build(configure-package): upd prepare-app-script

--- lib/eu/config/prepare-app-scripts/app-namespace.sh
+++ lib/eu/config/prepare-app-scripts/app-namespace.sh
@@ -5,3 +5,21 @@ I18NEXT_NAMESPACE=$1
 cat << _EOF_ > ./src/_constants/i18next/app-namespace.ts
 export const appNamespace = '${I18NEXT_NAMESPACE}';
 _EOF_
+
+
+
+cat << _EOF_ > ./src/_constants/i18next/i18next-constants.ts
+import i18next from 'i18next';
+import { appNamespace } from '@/_constants/i18next/app-namespace';
+
+export const backendErrorsSubnamespace = 'backend-errors';
+
+export const requestTranslateFunction = (
+  key: string,
+  options: Record<string, any>,
+) => i18next.t(`${appNamespace}:${backendErrorsSubnamespace}.${key}`, options);
+_EOF_
+
+
+
+
