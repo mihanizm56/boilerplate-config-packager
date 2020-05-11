8539426a530ebf865bdb08eb9d6d41bead604a3c
mihanizm56 <m727507.56@mail.ru>
2020-05-11 03:24:19 +0300

    build(configure-package): upd ver

--- lib/eu/config/prepare-app-scripts/app-namespace.sh
+++ lib/eu/config/prepare-app-scripts/app-namespace.sh
@@ -5,3 +5,7 @@ I18NEXT_NAMESPACE=$1
 cat << _EOF_ > src/_constants/i18next/app-namespace.ts
 export const appNamespace = '${I18NEXT_NAMESPACE}';
 _EOF_
+
+cat << _EOF_ > src/pages/home/_redux/ui-module/constants.ts
+export const reducerUIName = '@${I18NEXT_NAMESPACE}/ui-state';
+_EOF_
