9057604f03c4e0720f3578652d7a1b3eff0b0df9
mihanizm56 <m727507.56@mail.ru>
2020-08-04 02:12:25 +0300

    build(configure-package): update configs

--- lib/eu/config/deploy/nginx-envs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PATH_TO_NGINX=$1
-KLUSTER=$(printenv CLUSTER)
-
-sed -i -- "s/{{KLUSTER}}/$KLUSTER/g" $PATH_TO_NGINX
--- lib/eu/config/deploy/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nginx -g 'daemon off;' -c /etc/nginx/nginx.conf
