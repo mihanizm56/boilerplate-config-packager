53bffaaf2c6e056e0c87caab9a4dbfd1df930f0c
mihanizm56 <m727507.56@mail.ru>
2020-08-05 18:07:01 +0300

    build(configure-package): update

--- lib/eu/config/deploy/deploy-script.sh
+++ lib/eu/config/deploy/deploy-script.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
-PREV_TAG=$(git describe --abbrev=0 --tags)
-IFS='.'
-read -ra ADDR <<<"$PREV_TAG"
-
-LAST_INDEX=${ADDR[2]}
-
-MAJOR_VERSION=0
-MINOR_VERSION=0
-PATCH_VERSION=$((LAST_INDEX + 1))
-
-NEW_TAG="v${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}"
+source .env
 
 git add "."
 HUSKY_SKIP_HOOKS=1 git commit -m "update tag"
 
-git tag "${NEW_TAG}"
+git tag "${TAG}"
 
 git push --tags
 
 echo -en ""
-echo -en "Deployed tag: \e[40;1;42m $NEW_TAG \e[m\n"
+echo -en "Deployed tag: \e[40;1;42m $TAG \e[m\n"
 
 
 
