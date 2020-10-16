47d2a526aff77386656a870fd9a965518e41a3cc
mihanizm56 <m727507.56@mail.ru>
2020-10-16 06:26:42 +0300

    build(configure-package): update

--- config/packager/executor.js
+++ config/packager/executor.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const path = require('path');
+const colors = require('colors');
+const cliProgress = require('cli-progress');
 const Copier = require('@mihanizm56/node-file-copier');
 const { exec } = require('./utils/fs-promises');
 const { getConsoleArgs } = require('./utils/get-args');
@@ -35,23 +37,32 @@ const arrayToCopy = [
 
 const copier = new Copier({ arrayToCopy });
 
+const cliProgressBar = new cliProgress.SingleBar({
+  format: `CLI Progress |${colors.magenta(
+    '{bar}',
+  )}| {percentage}% || Config-packager execution`,
+  barCompleteChar: '\u2588',
+  barIncompleteChar: '\u2591',
+});
+
 const runPackage = async () => {
   try {
-    console.log('(config-packager): start to execute');
+    const cliRunner = cliProgressBar.create(100, 0, {
+      processName: 'directory preparation',
+    });
+    cliRunner.update(20);
 
     await exec('npm install @wildberries/boilerplate-config-packager');
-
-    console.log('(config-packager): start to copy');
+    cliRunner.update(40);
 
     copier.activate();
-
-    console.log('(config-packager): start to patch package.json');
+    cliRunner.update(60);
 
     await packageJsonPatch(configFolderPrefix);
-
-    console.log('(config-packager): package.json patched successfuly');
+    cliRunner.update(80);
 
     await exec('npm uninstall @wildberries/boilerplate-config-packager');
+    cliRunner.update(100);
   } catch (error) {
     console.log('error when executing the package', error);
     process.exit(1);
