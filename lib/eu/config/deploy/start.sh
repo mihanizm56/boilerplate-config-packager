cc8cda44c4180449b2b4d7104cd20190b8f4aa16
mihanizm56 <m727507.56@mail.ru>
2020-11-05 19:00:44 +0300

    build(configure-package): update

--- lib/eu/config/deploy/start.sh
+++ lib/eu/config/deploy/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bash /usr/share/metadata/core/config/deploy/runtime-envs/make-runtime-config.sh
+bash /usr/share/metadata/core/config/deploy/runtime-envs/production/make-runtime-config.sh
 
 nginx -g 'daemon off;' -c /etc/nginx/nginx.conf
