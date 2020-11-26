e4a74b458f1dc6dad3aa0ebe42592e3aae283bb1
mihanizm56 <m727507.56@mail.ru>
2020-11-26 23:30:42 +0300

    build(configure-package): update

--- config/packager/utils/source-file-parsers/gatsby/config.js
+++ config/packager/utils/source-file-parsers/gatsby/config.js
@@ -6,11 +6,21 @@ const pathToSource = path.join(process.cwd(), 'src');
 const pathToIndex = path.join(pathToSource, 'index.tsx');
 const pathToPagesFolder = path.join(pathToSource, 'pages');
 const pathToLayoutsFolder = path.join(pathToSource, '_layouts');
+const pathToTypesFile = path.join(pathToSource, 'react-app-env.d.ts');
+const pathToEnzymeFile = path.join(pathToSource, 'setupEnzyme.ts');
+const pathToProxyFile = path.join(pathToSource, 'setupProxy.js');
 
 // create sources
 const pathToLocalSources = path.join(__dirname, 'sources');
 
 module.exports = {
-  removeSources: [pathToIndex, pathToPagesFolder, pathToLayoutsFolder],
+  removeSources: [
+    pathToIndex,
+    pathToPagesFolder,
+    pathToLayoutsFolder,
+    pathToTypesFile,
+    pathToEnzymeFile,
+    pathToProxyFile,
+  ],
   createSources: [{ from: pathToLocalSources, to: pathToSource }],
 };
