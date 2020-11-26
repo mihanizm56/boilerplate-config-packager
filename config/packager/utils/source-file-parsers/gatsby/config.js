7f11fb83d2cf1374c0c95b721429880a18d6279e
mihanizm56 <m727507.56@mail.ru>
2020-11-26 22:49:36 +0300

    build(configure-package): update

--- config/packager/utils/source-file-parsers/gatsby/config.js
+++ config/packager/utils/source-file-parsers/gatsby/config.js
@@ -9,14 +9,7 @@ const pathToLayoutsFolder = path.join(pathToSource, '_layouts');
 const pathToUtilsFolder = path.join(process.cwd(), 'utils');
 
 // create sources
-const pathToLocalIndex = path.join(__dirname, 'sources', 'index.tsx');
-const pathToLocalPages = path.join(__dirname, 'sources', '_pages');
-const pathToLocalGatsbyPages = path.join(__dirname, 'sources', 'pages');
-const pathToLocalI18n = path.join(__dirname, 'sources', 'i18n');
-const pathToLocalStatic = path.join(__dirname, 'sources', 'static');
-const pathToLocalLayouts = path.join(__dirname, 'sources', '_layouts');
-const pathToLocalHtml = path.join(__dirname, 'sources', 'html.js');
-const pathTLocalUtilsFolder = path.join(__dirname, 'sources', 'utils');
+const pathToLocalSources = path.join(__dirname, 'sources');
 
 module.exports = {
   removeSources: [
@@ -25,14 +18,5 @@ module.exports = {
     pathToLayoutsFolder,
     pathToUtilsFolder,
   ],
-  createSources: [
-    { from: pathToLocalIndex, to: path.join(pathToSource, 'index.tsx') },
-    { from: pathToLocalPages, to: path.join(pathToSource, '_pages') },
-    { from: pathToLocalGatsbyPages, to: path.join(pathToSource, 'pages') },
-    { from: pathToLocalI18n, to: path.join(pathToSource, 'i18n') },
-    { from: pathToLocalStatic, to: path.join(pathToSource, 'static') },
-    { from: pathToLocalLayouts, to: path.join(pathToSource, '_layouts') },
-    { from: pathToLocalHtml, to: path.join(pathToSource, 'html.js') },
-    { from: pathTLocalUtilsFolder, to: path.join(process.cwd(), 'utils') },
-  ],
+  createSources: [{ from: pathToLocalSources, to: pathToSource }],
 };
