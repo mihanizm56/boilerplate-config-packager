c77470837529b4fc5b7874fee1016835568fd190
mihanizm56 <m727507.56@mail.ru>
2020-11-27 04:17:36 +0300

    build(configure-package): update

--- config/packager/utils/source-file-parsers/graphql/config.js
+++ config/packager/utils/source-file-parsers/graphql/config.js
@@ -4,12 +4,19 @@ const pathToSource = path.join(process.cwd(), 'src');
 
 // remove sources
 const pathToIndex = path.join(pathToSource, 'index.tsx');
-const pathToPagesFolder = path.join(pathToSource, '_redux');
+const pathToReduxFolder = path.join(pathToSource, '_redux');
+const pathToPagesFolder = path.join(pathToSource, 'pages');
+const pathToLayoutsFolder = path.join(pathToSource, '_layouts');
 
 // create sources
 const pathToLocalSources = path.join(__dirname, 'sources');
 
 module.exports = {
-  removeSources: [pathToIndex, pathToPagesFolder],
+  removeSources: [
+    pathToIndex,
+    pathToPagesFolder,
+    pathToReduxFolder,
+    pathToLayoutsFolder,
+  ],
   createSources: [{ from: pathToLocalSources, to: pathToSource }],
 };
