0e8e42859dcc7e45549503f32a3dc3e899283596
mihanizm56 <m727507.56@mail.ru>
2020-11-27 03:59:47 +0300

    build(configure-package): update

--- config/packager/utils/patch-source-Files.js
+++ config/packager/utils/patch-source-Files.js
@@ -1,11 +1,12 @@
 const { processSourceFilesGatsby } = require('./source-file-parsers/gatsby');
+const { processSourceFilesGraphql } = require('./source-file-parsers/graphql');
 
 module.exports.patchSourceFiles = async flags => {
   if (flags.gatsby) {
     await processSourceFilesGatsby();
   }
 
-  //   if (flags.graphql) {
-  //     await processSourceFilesGraphql();
-  //   }
+  if (flags.graphql) {
+    await processSourceFilesGraphql();
+  }
 };
--- config/packager/utils/source-file-parsers/graphql/index.js
+++ config/packager/utils/source-file-parsers/graphql/index.js
@@ -4,7 +4,7 @@ const { removeSources, createSources } = require('./config');
 
 const copier = new Copier({ arrayToCopy: createSources });
 
-module.exports.processSourceFilesGatsby = async () => {
+module.exports.processSourceFilesGraphql = async () => {
   // remove all unnecessary files and folders
   await Promise.all(removeSources.map(deletePath => remove(deletePath)));
 
